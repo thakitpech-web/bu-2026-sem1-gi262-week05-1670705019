7c8169f1c5c2e85c6d245003abab9c5be879124c
thakitttt <thakit.pech@bumail.net>
2026-09-15 10:55:06 +0700

    all done

--- Assets/Workshop/Student/Scripts/OOPPlayer.cs
+++ Assets/Workshop/Student/Scripts/OOPPlayer.cs
@@ -43,9 +43,20 @@ namespace Solution
 
         public void UseFireStorm()
         {
-            if (inventory.HasItem("FireStorm",1))
+            if (inventory.HasItem("FireStorm", 1))
             {
                 //stundent exercise: use FireStorm to attack 3 lower energy enemies on map
+                inventory.UseItem("FireStorm", 1);
+                OOPEnemy[] enemies = SortEnemiesByRemainningEnergy1();
+                int count = 3;
+                if (count > enemies.Length)
+                {
+                    count = enemies.Length;
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    enemies[i].TakeDamage(10);
+                }
             }
             else
             {
@@ -56,7 +67,18 @@ namespace Solution
         {
             var enemies = mapGenerator.GetEnemies();
             //stundent exercise: sort enemies by remainning energy
-
+            for (int i = 0; i < enemies.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < enemies.Length; j++)
+                {
+                    if (enemies[j].energy < enemies[minIndex].energy)
+                    {
+                        minIndex = j;
+                    }
+                }
+                (enemies[i], enemies[minIndex]) = (enemies[minIndex], enemies[i]);
+            }
             return enemies;
         }
 
@@ -64,6 +86,7 @@ namespace Solution
         {
             var enemies = mapGenerator.GetEnemies();
             //stundent exercise: sort enemies by remainning energy
+            Array.Sort(enemies, (a, b) => a.energy.CompareTo(b.energy));
 
             return enemies;
         }
