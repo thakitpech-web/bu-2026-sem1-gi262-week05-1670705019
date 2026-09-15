fdef9b272e0de5e728769b3aec24ddea6da47340
thakitttt <thakit.pech@bumail.net>
2026-09-15 10:21:57 +0700

    test

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -11,16 +11,69 @@ namespace Assignment
         #region Lecture
         public int[] LCT01_SelectionSortAscending(int[] numbers)
         {
+            int n = numbers.Length;
+            for(int i = 0; i < n-1; i++)
+            {
+                int minIndex = i;
+                for(int j = i+1; j < n; j++)
+                {
+                    if (numbers[i] < numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = numbers[minIndex];
+                numbers[minIndex] = numbers[i];
+                numbers[i] = temp;
+                //(numbers[i], numbers[minIndex]) = (numbers[minIndex], numbers[i]);
+            }
+            foreach (var n_ in numbers)
+            {
+                Debug.Log(n_);
+            }
             return numbers;
         }
 
         public int[] LCT02_BubbleSortAscending(int[] numbers)
         {
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            foreach (var n_ in numbers)
+            {
+                Debug.Log(n_);
+            }
             return numbers;
         }
 
         public int[] LCT03_InsertionSortAscending(int[] numbers)
         {
+            int n = numbers.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (j >= 0 && numbers[j] > key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
+            foreach (var n_ in numbers)
+            {
+                Debug.Log(n_);
+            }
             return numbers;
         }
 
@@ -35,12 +88,14 @@ namespace Assignment
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
-            return numbers;
+            
+            return numbers; 
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
